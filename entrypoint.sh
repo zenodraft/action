9bccd5b1bf50517aa2128428be50d7f6cb71dbce
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-11 14:13:38 +0200

    entrypoint syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "INPUT_IN_COLLECTION = $INPUT_IN_COLLECTION"
 echo "INPUT_SANDBOX = $INPUT_SANDBOX"
 
-if [ "$INPUT_SANDBOX" = "false"]
+if [ "$INPUT_SANDBOX" = "false" ]
 then
     SANDBOX=""
 else
@@ -15,8 +15,8 @@ if [ -z "$INPUT_COLLECTION" ]
 then
     LATEST_ID=$(zenodraft $SANDBOX deposition create in-new-collection)
 else
-    zenodraft $SANDBOX deposition create in-existing-collection $INPUT_IN_COLLECTION
-    LATEST_ID=$(zenodraft $SANDBOX deposition latest $INPUT_IN_COLLECTION)
+    zenodraft $SANDBOX deposition create in-existing-collection "$INPUT_IN_COLLECTION"
+    LATEST_ID=$(zenodraft $SANDBOX deposition latest "$INPUT_IN_COLLECTION")
 fi
 
 
