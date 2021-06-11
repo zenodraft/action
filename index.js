24be270ed8517ecbe564fd8d3f57b6dcc5c52b0f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-11 12:06:07 +0200

    iife

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const {getInput,setFailed} = require('@actions/core');
-const {exec} = require('@actions/exec');
-
-
-try {
-    const collection_id = getInput('in-collection');
-    const sandbox = getInput('sandbox') === 'true' ? true : false;
+(async () => {
+    const {getInput,setFailed} = require('@actions/core');
+    const {exec} = require('@actions/exec');
     
-    if (collection_id !== '') {
-        console.log(`Going to be publishing in collection ${collection_id}.`);
-        await exec('zenodraft', ['--sandbox', 'deposition', 'create', 'in-collection', collection_id])
-    } else {
-        console.log(`Going to be publishing in new collection.`);
+    try {
+        const collection_id = getInput('in-collection');
+        const sandbox = getInput('sandbox') === 'true' ? true : false;
+        
+        if (collection_id !== '') {
+            console.log(`Going to be publishing in collection ${collection_id}.`);
+            await exec('zenodraft', ['--sandbox', 'deposition', 'create', 'in-collection', collection_id])
+        } else {
+            console.log(`Going to be publishing in new collection.`);
+        }
+    
+        if (sandbox) {
+            console.log(`Going to be publishing on Zenodo Sandbox.`);
+        } else {
+            console.log(`Going to be publishing on Zenodo`);
+        }
+    
+        console.log(zenodraft);
+    
+    } catch (error) {
+        setFailed(error.message);
     }
-
-    if (sandbox) {
-        console.log(`Going to be publishing on Zenodo Sandbox.`);
-    } else {
-        console.log(`Going to be publishing on Zenodo`);
-    }
-
-    console.log(zenodraft);
-
-} catch (error) {
-    setFailed(error.message);
-}
+})()
