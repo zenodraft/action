3dd2cb0008ca96371a28e87e5d5f5650c70bd3c8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-11 11:35:51 +0200

    fiddling with action

---
+++ index.js
@@ -0,0 +1,25 @@
+import {getInput,setFailed} from '@actions/core';
+import {context} from '@actions/github';
+
+try {
+    const collection_id = getInput('in-collection');
+    const sandbox = getInput('sandbox') === 'true' ? true : false;
+    
+    if (collection_id !== '') {
+        console.log(`Going to be publishing in collection ${collection_id}.`);
+    } else {
+        console.log(`Going to be publishing in new collection.`);
+    }
+
+    if (sandbox) {
+        console.log(`Going to be publishing on Zenodo Sandbox.`);
+    } else {
+        console.log(`Going to be publishing on Zenodo`);
+    }
+
+
+    const payload = JSON.stringify(context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    setFailed(error.message);
+}
