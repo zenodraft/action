8d669ef1d8ef38431bdcff65f16f46a9cf3cd575
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-11 14:02:30 +0200

    creating now part of action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,21 @@
 echo "INPUT_IN_COLLECTION = $INPUT_IN_COLLECTION"
 echo "INPUT_SANDBOX = $INPUT_SANDBOX"
 
-# [ -z "$INPUT_SANDBOX" ] && SANDBOX="" || SANDBOX="--sandbox"
-
-# if [ -z "$INPUT_IN_COLLECTION" ]
-# then
-#     LATEST_ID=$(zenodraft $SANDBOX deposition create in-new-collection)
-# else
-#     zenodraft $SANDBOX deposition create in-existing-collection $INPUT_IN_COLLECTION
-#     LATEST_ID=$(zenodraft $SANDBOX deposition latest $INPUT_IN_COLLECTION)
-# fi
+if [ "$INPUT_SANDBOX" == "false"]
+then
+    SANDBOX=""
+else
+    SANDBOX="--sandbox"
+fi
 
 
-
+if [ -z "$INPUT_IN_COLLECTION" ]
+then
+    LATEST_ID=$(zenodraft $SANDBOX deposition create in-new-collection)
+else
+    zenodraft $SANDBOX deposition create in-existing-collection $INPUT_IN_COLLECTION
+    LATEST_ID=$(zenodraft $SANDBOX deposition latest $INPUT_IN_COLLECTION)
+fi
 
 
 # zenodraft $SANDBOX file add $LATEST_ID 2021-test.pdf
