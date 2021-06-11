7af34675197c69d26daccac2a5232a9b9df6923e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-11 13:52:13 +0200

    entrypoint action debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,21 @@
-#!/bin/bash
-[ -z "$INPUT_ARGS" ] && zenodraft --help || zenodraft $INPUT_ARGS
+#!/bin/bash -x
+
+echo "INPUT_IN_COLLECTION = $INPUT_IN_COLLECTION"
+echo "INPUT_SANDBOX = $INPUT_SANDBOX"
+
+# [ -z "$INPUT_SANDBOX" ] && SANDBOX="" || SANDBOX="--sandbox"
+
+# if [ -z "$INPUT_IN_COLLECTION" ]
+# then
+#     LATEST_ID=$(zenodraft $SANDBOX deposition create in-new-collection)
+# else
+#     zenodraft $SANDBOX deposition create in-existing-collection $INPUT_IN_COLLECTION
+#     LATEST_ID=$(zenodraft $SANDBOX deposition latest $INPUT_IN_COLLECTION)
+# fi
+
+
+
+
+
+# zenodraft $SANDBOX file add $LATEST_ID 2021-test.pdf
+# zenodraft $SANDBOX metadata update $LATEST_ID .zenodo.json
