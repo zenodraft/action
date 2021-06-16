e3adf9ad3a1ea15a9f903d3900dfeb1c469315bd
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-16 15:18:35 +0200

    not calling the binary for now

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ export const main = async (): Promise<void> => {
     try {
         const collection_id = getInput('collection');
         const sandbox = getInput('sandbox') === 'false' ? false : true;
-        const zenodraft = 'node_modules/zenodraft/bin/index.js';
         const verbose = false;
         
         let latest_id;
@@ -23,7 +22,6 @@ export const main = async (): Promise<void> => {
             latest_id = await create_empty_deposition_in_existing_collection(sandbox, collection_id, verbose)
         }
         await add_file_to_deposition(sandbox, latest_id, 'test.txt', verbose);
-        await exec(zenodraft, [sandbox, 'metadata', 'update', latest_id, '.zenodo.json']);
         await update_deposition_metadata(sandbox, latest_id, '.zenodo.json', verbose);
     } catch (error) {
         setFailed(error.message);
