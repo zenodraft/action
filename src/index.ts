c40548aa5d32d56bc4a0651960cf994222efddd2
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-22 16:52:25 +0200

    updated names of called function after upgrade to zenodraft==0.7.0

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ export const main = async (): Promise<void> => {
         
         let latest_id;
         if (collection_id === '') {
-            latest_id = await zenodraft.create_empty_deposition_in_new_collection(sandbox, verbose)
+            latest_id = await zenodraft.deposition_create_in_new_collection(sandbox, verbose)
         } else {
-            latest_id = await zenodraft.create_empty_deposition_in_existing_collection(sandbox, collection_id, verbose)
+            latest_id = await zenodraft.deposition_create_in_existing_collection(sandbox, collection_id, verbose)
         }
-        await zenodraft.add_file_to_deposition(sandbox, latest_id, 'test.txt', verbose);
-        await zenodraft.update_deposition_metadata(sandbox, latest_id, '.zenodo.json', verbose);
+        await zenodraft.file_add(sandbox, latest_id, 'test.txt', verbose);
+        await zenodraft.metadata_update(sandbox, latest_id, '.zenodo.json', verbose);
     } catch (error) {
         setFailed(error.message);
     }
