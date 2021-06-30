61367630edd9b33c1a08d648e7fba595501067ed
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-30 19:42:19 +0200

    changed the way dates are written back to the CFF when upserting; added tests for date handling in 1.1.0

--- src/upserting.ts
+++ src/upserting.ts
@@ -144,7 +144,19 @@ export const upsert_prereserved_doi = (upsert_location: string, prereserved_doi:
 
 
 const write_cff_file = (cff: CffObject): void => {
-    const cffstr = yaml.dump(cff, { sortKeys: false })
+    let cffstr = yaml.dump(cff, { sortKeys: false })
+    // replace full precision dates with just YYYY-MM-DD before writing to file
+    const regexes = [
+        new RegExp('(?<k>date-accessed): (?<v>[0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00.000Z', 'g'),
+        new RegExp('(?<k>date-downloaded): (?<v>[0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00.000Z', 'g'),
+        new RegExp('(?<k>date-published): (?<v>[0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00.000Z', 'g'),
+        new RegExp('(?<k>date-released): (?<v>[0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00.000Z', 'g'),
+        new RegExp('(?<k>date-end): (?<v>[0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00.000Z', 'g'),
+        new RegExp('(?<k>date-start): (?<v>[0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00.000Z', 'g')
+    ]
+    for (const regex of regexes) {
+        cffstr = cffstr.replace(regex, '$<k>: $<v>')
+    }
     fs.writeFileSync('CITATION.cff', cffstr, 'utf8')
     return
 }
