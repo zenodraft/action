2949afca8308fe1774a853e1bf1bc72edef8a528
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-07-01 10:37:42 +0200

    casting to ReleaseEvent

--- src/github.ts
+++ src/github.ts
@@ -1,11 +1,14 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { WorkflowDispatchEvent } from '@octokit/webhooks-definitions/schema'
+import { WorkflowDispatchEvent, ReleaseEvent } from '@octokit/webhooks-definitions/schema'
 
 
 export const show_github_payload = (): void => {
     if (github.context.eventName === 'workflow_dispatch') {
         const payload = github.context.payload as WorkflowDispatchEvent
+        core.info(JSON.stringify(payload, null, 4))
+    } else if (github.context.eventName === 'release') {
+        const payload = github.context.payload as ReleaseEvent
         core.info(JSON.stringify(payload, null, 4))
     } else {
         core.info(`unsupported event: ${github.context.eventName}`)
