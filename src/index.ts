a1259737cd4364552b975b327b6aa56f29ddacb9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-07-05 12:28:46 +0200

    wrapped the files part in a group

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { exec } from '@actions/exec'
-import { getInput,setFailed } from '@actions/core'
+import * as core from '@actions/core'
 import { get_payload as validate_triggering_event } from './releasing/'
 import { update_github_state } from './releasing/'
 import { upsert_prereserved_doi } from './upserting/'
@@ -12,15 +12,15 @@ export const main = async (): Promise<void> => {
 
     try {
 
-        const collection_id = getInput('collection')
-        const compression = getInput('compression')
-        const filenames = getInput('filenames')
-        const metadata = getInput('metadata')
-        const publish = getInput('publish') === 'true' ? true : false
-        const sandbox = getInput('sandbox') === 'false' ? false : true
-        const upsert_doi = getInput('upsert-doi') === 'true' ? true : false
-        const upsert_location = getInput('upsert-location')
-        const verbose = getInput('verbose') === 'true' ? true : false
+        const collection_id = core.getInput('collection')
+        const compression = core.getInput('compression')
+        const filenames = core.getInput('filenames')
+        const metadata = core.getInput('metadata')
+        const publish = core.getInput('publish') === 'true' ? true : false
+        const sandbox = core.getInput('sandbox') === 'false' ? false : true
+        const upsert_doi = core.getInput('upsert-doi') === 'true' ? true : false
+        const upsert_location = core.getInput('upsert-location')
+        const verbose = core.getInput('verbose') === 'true' ? true : false
 
         assert(['tar.gz', 'zip'].includes(compression), 'Invalid value for input argument \'compression\'.' )
         assert((new RegExp('[0-9]+')).test(collection_id) || collection_id === '', 'Invalid value for input argument \'collection\'.')
@@ -42,6 +42,7 @@ export const main = async (): Promise<void> => {
             upsert_prereserved_doi(upsert_location, prereserved_doi)
         }
 
+        core.startGroup('Adding files')
         // upload only the files specified in the filenames argument, or
         // upload a snapshot of the complete repository
         if (filenames === '') {
@@ -61,6 +62,7 @@ export const main = async (): Promise<void> => {
                 await zenodraft.file_add(sandbox, latest_id, filename, verbose)
             }
         }
+        core.endGroup()
 
         // update the metadata if the user has specified a filename that contains metadata
         if (metadata !== '') {
@@ -74,7 +76,7 @@ export const main = async (): Promise<void> => {
         await update_github_state(payload, upsert_doi, metadata)
 
     } catch (error) {
-        setFailed(error.message)
+        core.setFailed(error.message)
     }
 }
 
