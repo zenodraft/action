0da908ebddca61679507f409e1301edff3be8b10
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2024-01-30 13:29:05 +0100

    updated how zenodraft is imported into the action; made updates to account for updated zenodraft interface; renamed 'collection' vars to 'concept' vars

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,33 @@ import { get_payload } from './releasing/'
 import { update_github_state } from './releasing/'
 import { upsert_prereserved_doi } from './upserting/'
 import assert from 'assert'
-import zenodraft from 'zenodraft'
+import { default as zenodraft } from 'zenodraft'
 
 
 const read_inputs = () => {
-    const collection_id = core.getInput('collection')
+    const concept_id = core.getInput('concept')
     const compression = core.getInput('compression')
     const filenames = core.getInput('filenames')
     const metadata = core.getInput('metadata')
     const publish = core.getInput('publish') === 'true' ? true : false
     const sandbox = core.getInput('sandbox') === 'false' ? false : true
+    const token = sandbox ? process.env.ZENODO_SANDBOX_ACCESS_TOKEN || '' : process.env.ZENODO_ACCESS_TOKEN || ''
     const upsert_doi = core.getInput('upsert-doi') === 'true' ? true : false
     const upsert_location = core.getInput('upsert-location')
     const verbose = core.getInput('verbose') === 'true' ? true : false
 
+
     assert(['tar.gz', 'zip'].includes(compression), 'Invalid value for input argument \'compression\'.' )
-    assert((new RegExp('[0-9]+')).test(collection_id) || collection_id === '', 'Invalid value for input argument \'collection\'.')
+    assert((new RegExp('[0-9]+')).test(concept_id) || concept_id === '', 'Invalid value for input argument \'concept\'.')
 
     return {
-        collection_id,
+        concept_id,
         compression,
         filenames,
         metadata,
         publish,
         sandbox,
+        token,
         upsert_doi,
         upsert_location,
         verbose
@@ -42,12 +45,13 @@ export const main = async (): Promise<void> => {
 
         core.startGroup('processing user input')
         const {
-            collection_id,
+            concept_id,
             compression,
             filenames,
             metadata,
             publish,
             sandbox,
+            token,
             upsert_doi,
             upsert_location,
             verbose
@@ -59,20 +63,20 @@ export const main = async (): Promise<void> => {
         const payload = await get_payload(metadata)
         core.endGroup()
 
-        // create the deposition as a new version in a new collection or
-        // as a new version in an existing collection:
+        // create the deposition as a new version in a new concept or
+        // as a new version in an existing concept:
         core.startGroup(`creating deposition on ${sandbox === true ? 'Zenodo Sandbox' : 'Zenodo'}`)
-        let latest_id;
-        if (collection_id === '') {
-            latest_id = await zenodraft.deposition_create_in_new_collection(sandbox, verbose)
+        let version_id;
+        if (concept_id === '') {
+            version_id = await zenodraft.deposition_create_concept(token, sandbox, verbose)
         } else {
-            latest_id = await zenodraft.deposition_create_in_existing_collection(sandbox, collection_id, verbose)
+            version_id = await zenodraft.deposition_create_version(token, sandbox, concept_id, verbose)
         }
         core.endGroup()
 
         if (upsert_doi === true) {
             core.startGroup('upserting doi')
-            const prereserved_doi = await zenodraft.deposition_show_prereserved(sandbox, latest_id, verbose)
+            const prereserved_doi = await zenodraft.deposition_show_prereserved(token, sandbox, version_id, verbose)
             upsert_prereserved_doi(upsert_location, prereserved_doi)
             core.endGroup()
         }
@@ -85,16 +89,16 @@ export const main = async (): Promise<void> => {
             if (compression === 'tar.gz') {
                 await exec('touch', [archive_name])
                 await exec('tar', ['--exclude=.git', `--exclude=${archive_name}`, '-zcvf', archive_name, '.'])
-                await zenodraft.file_add(sandbox, latest_id, archive_name, verbose)
+                await zenodraft.file_add(token, sandbox, version_id, archive_name, verbose)
             } else if (compression === 'zip') {
                 await exec('zip', ['-r', '-x', '/.git*', '-v', archive_name, '.'])
-                await zenodraft.file_add(sandbox, latest_id, archive_name, verbose)
+                await zenodraft.file_add(token, sandbox, version_id, archive_name, verbose)
             } else {
                 throw new Error('Unknown compression method.')
             }
         } else {
             for (const filename of filenames.split(' ')) {
-                await zenodraft.file_add(sandbox, latest_id, filename, verbose)
+                await zenodraft.file_add(token, sandbox, version_id, filename, verbose)
             }
         }
         core.endGroup()
@@ -102,13 +106,13 @@ export const main = async (): Promise<void> => {
         // update the metadata if the user has specified a filename that contains metadata
         if (metadata !== '') {
             core.startGroup('adding metadata')
-            await zenodraft.metadata_update(sandbox, latest_id, metadata, verbose)
+            await zenodraft.metadata_update(token, sandbox, version_id, metadata, verbose)
             core.endGroup() 
         }
 
         if (publish === true) {
             core.startGroup('publishing')
-            await zenodraft.deposition_publish(sandbox, latest_id, verbose)
+            await zenodraft.deposition_publish(token, sandbox, version_id, verbose)
             core.endGroup()
         }
 
